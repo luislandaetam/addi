4047ff4b0d612f695c00ba456e294d1bb69045a5
Luis Landaeta <luis.landaeta24@gmail.com>
2023-07-09 19:47:10 -0400

    faqs

--- src/app/components/FAQs/FAQs.jsx
+++ src/app/components/FAQs/FAQs.jsx
@@ -1,3 +1,12 @@
+'use client'
+
+import { Box, Typography } from '@mui/material'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { AskedQuestion } from '../AskedQuestion/AskedQuestion'
+
 export function FAQs() {
   return (
     <article className='mt-10'>
@@ -7,81 +16,74 @@ export function FAQs() {
       <h3 className='text-home-black-letters text-center font-bold text-4xl'>
         Estas son las preguntas más frecuentes.
       </h3>
-      <section className='mx-60'>
-        <div className='my-8'>
-          <h3 className='text-[#0566FE] my-4'>
-            ¿Cómo puedo obtener leads desde ADDI?
-          </h3>
-          <p>
-            ADDI tiene los "formularios conversacionales" incorporados en el
-            panel de control, los cuales son los suficientemente inteligentes
-            como para almacenar la información de tus usuarios en una hoja de
-            cálculo o en un CRM.
-          </p>
+      <Box m='20px'>
+        <div>
+          <AskedQuestion
+            question='¿Cómo puedo obtener leads desde ADDI?'
+            answer='ADDI tiene los "formularios conversacionales" incorporados en el panel
+          de control, los cuales son los suficientemente inteligentes como para
+          almacenar la información de tus usuarios en una hoja de cálculo o en
+          un CRM.'
+          />
         </div>
-        <div className='my-8'>
-          <h3 className='text-[#0566FE] my-4'>¿Recibo soporte de por vida?</h3>
-          <p>
-            El soporte al cliente de por vida está disponible de forma gratuita
-            para este producto. Sin embargo, el soporte para la construcción del
-            chatbot también está disponible de por vida, pero es de pago o viene
-            incluido en un plan hecho para ti.
-          </p>
+
+        <div>
+          <AskedQuestion
+            question='¿Recibo soporte de por vida?'
+            answer='El soporte al cliente de por vida está disponible de forma
+          gratuita para este producto. Sin embargo, el soporte para la
+          construcción del chatbot también está disponible de por vida, pero
+          es de pago o viene incluido en un plan hecho para ti.'
+          />
         </div>
-        <div className='my-8'>
-          <h3 className='text-[#0566FE] my-4'>
-            ¿Cómo puedo utilizar a ADDI para automatizar el servicio al cliente?
-          </h3>
-          <p>
-            ADDI es un chatbot el cual funciona como el punto de contacto
-            inicial del departamento de servicio al cliente y resolver consultas
-            comunes. Los clientes pueden recibir asistencia las 24 horas del
-            día, los 7 días de la semana y a través de diversos medios de
-            comunicación.
-          </p>
+
+        <div>
+          <AskedQuestion
+            question='¿Cómo puedo utilizar a ADDI para automatizar el servicio al
+              cliente?'
+            answer='ADDI es un chatbot el cual funciona como el punto de contacto
+              inicial del departamento de servicio al cliente y resolver
+              consultas comunes. Los clientes pueden recibir asistencia las 24
+              horas del día, los 7 días de la semana y a través de diversos
+              medios de comunicación.'
+          />
         </div>
-        <div className='my-8'>
-          <h3 className='text-[#0566FE] my-4'>
-            ¿ADDI tiene un chatbot en Messenger?
-          </h3>
-          <p>
-            Dado que ADDI es una plataforma de chatbots omnicanal, esta puede
-            integrarse un chatbot en Facebook Messenger, así como en WhatsApp,
-            Instagram, WordPress, el sitio web, y mensajes de texto (SMS) .
-          </p>
+
+        <div>
+          <AskedQuestion
+            question='¿ADDI tiene un chatbot en Messenger?'
+            answer='Dado que ADDI es una plataforma de chatbots omnicanal, esta puede
+          integrarse un chatbot en Facebook Messenger, así como en WhatsApp,
+          Instagram, WordPress, el sitio web, y mensajes de texto (SMS).'
+          />
         </div>
-        <div className='my-8'>
-          <h3 className='text-[#0566FE] my-4'>¿Cuál es el precio?</h3>
-          <p className='my-4'>Ofrecemos 4 planes:</p>
-          <ul>
-            <li>Basic: USD 50 Mensual - USD 500 Anual (2 Meses Gratis) </li>
-            <li>
-              Intermediate: USD 80 Mensual - USD 800 Anual (2 Meses Gratis)
-            </li>
-            <li>Advanced: USD 100 Mensual - USD 1000 Anual (2 Meses Gratis)</li>
-            <li className='mt-4'>
-              Plan Premium for Agencies, donde posees bots ilimitados: USD 150
-              Mensual - USD 1500 Anual (2 Meses Gratis)
-            </li>
-          </ul>
-          <p className='mt-4'>
-            Ya sea que seas dueño de un pequeño negocio o una gran empresa, ADDI
-            puede ayudarte a llevar tu interacción con los clientes al siguiente
-            nivel. Para obtener más información, consulta nuestra lista de
-            precios.
-          </p>
+
+        <div>
+          <AskedQuestion
+            question='¿Cuál es el precio?'
+            answer='Ofrecemos 4 planes:'
+            bullets={[
+              'Basic: USD 50 Mensual - USD 500 Anual (2 Meses Gratis)',
+              ' Intermediate: USD 80 Mensual - USD 800 Anual (2 Meses Gratis)',
+              'Advanced: USD 100 Mensual - USD 1000 Anual (2 Meses Gratis)',
+              'Plan Premium for Agencies, donde posees bots ilimitados: USD 150 Mensual - USD 1500 Anual (2 Meses Gratis)'
+            ]}
+            answer2='Ya sea que seas dueño de un pequeño negocio o una gran empresa,
+          ADDI puede ayudarte a llevar tu interacción con los clientes al
+          siguiente nivel. Para obtener más información, consulta nuestra
+          lista de precios.'
+          />
         </div>
-        <div className='my-8'>
-          <div className='flex flex-row justify-between'>
-            <h3 className='text-[#0566FE] my-4'>¿Tienen un plan gratuito?</h3>
-          </div>
-          <p>
-            No, no ofrecemos un plan gratuito, pero ofrecemos una prueba
-            gratuita de 7 días donde tienes acceso a todo el panel y soporte
-            completo al cliente.
-          </p>
+
+        <div>
+          <AskedQuestion
+            question='¿Tienen un plan gratuito?'
+            answer='No, no ofrecemos un plan gratuito, pero ofrecemos una prueba
+          gratuita de 7 días donde tienes acceso a todo el panel y soporte
+          completo al cliente.'
+          />
         </div>
-      </section>
+      </Box>
     </article>
   )
 }
